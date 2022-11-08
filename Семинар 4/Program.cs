0b8d6c05c7b6db61a94d5ae18ea2663ad78bea24
Daria <tsvikevich.daria@gmail.com>
2022-11-08 12:21:51 +0300

    Seminar 4 added

---
+++ Семинар 4/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+/*int FindSum (int number)
+{
+    int  sum = 0;
+
+    for(int current = 1; current <= number; current++)
+        sum += current;
+
+    return sum;
+}
+Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of numbers between 1 and {a} is {FindSum(a)}");
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*int Find (int number)
+{
+    int digit = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        digit++;
+    }
+    return digit;
+}
+Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The number of digits in a number {a} is {Find(a)}");
+*/
+
+//Написать программу, которая определит произведение чисел от 1 до N.
+/*int FindCompos (int number)
+{
+    int  mult = 1;
+
+    for(int current = 1; current <= number; current++)
+        mult *= current;
+
+    return mult;
+}
+Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Composition of numbers between 1 and {a} is {FindCompos(a)}");
+*/
